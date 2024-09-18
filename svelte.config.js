4dc4053d31c2135d3f96a96cc2ee692085a319fd
zhou jian <1099738693@qq.com>
2024-09-18 16:49:14 +0800

    去掉可及性检测

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,10 @@ const config = {
             fallback: 'index.html',
         }),
     },
+    onwarn: (warning, handler) => {
+        if (warning.code.startsWith('a11y-')) return
+        handler(warning)
+    },
 }
 
 export default config
