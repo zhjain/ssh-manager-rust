a4fcc6e9c412d48102fbc87384147163b5f346d9
zhou jian <1099738693@qq.com>
2024-09-20 17:05:48 +0800

    前端获取轮询结果

--- src/routes/connections/[id]/+page.svelte
+++ src/routes/connections/[id]/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-    // import { page } from "$app/stores"
-    // import connectionStore from "$lib/store/connectionStore"
-    // 获取路由参数
-    // let { id } = $page.params
+    import { listen } from "@tauri-apps/api/event"
 
     import { onMount } from "svelte"
-    import type { PageData } from "./$types"
+    import { page } from "$app/stores"
 
-    export let data: PageData
+    import connectionStore from "$lib/store/connectionStore"
+    // 获取路由参数
+    let id = Number($page.params.id)
 
     let connectionStatus: {
         status: string
@@ -19,23 +18,40 @@
     let error: string | null = null
 
     onMount(() => {
-        connectionStatus = data.connectionStatus
-        error = data.error
+        connectionStatus = $connectionStore.serverStatus[id]
+        // error = data.error
+        // 监听某个 SSH 连接的事件
+        listenToConnection(id)
     })
+
+    function listenToConnection(connectionId: number) {
+        listen<Response<ServerStatus>>(
+            `server-status-update-${connectionId}`,
+            event => {
+                // 保存接收到的服务器状态
+                $connectionStore.serverStatus[connectionId] = event.payload.data
+
+                // 如果是当前显示的连接，更新UI
+                if ($connectionStore.current.id === connectionId) {
+                    connectionStatus = event.payload.data
+                }
+            },
+        )
+    }
 </script>
 
 <div>
     <div class="border-b">
-        {connectionStatus?.status}
+        {connectionStatus?.status || ""}
     </div>
     <div class="border-b">
-        {connectionStatus?.uptime}
+        {connectionStatus?.uptime || ""}
     </div>
     <div class="border-b">
-        {connectionStatus?.cpu_usage}
+        {JSON.stringify(connectionStatus?.cpu_usage) || ""}
     </div>
     <div class="border-b">
-        {connectionStatus?.memory_usage}
+        {JSON.stringify(connectionStatus?.memory_usage) || ""}
     </div>
 
     {error}
